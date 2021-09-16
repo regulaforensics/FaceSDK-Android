7ffe38e01c144e9e8edfdbd85de8865a6989fafc
Sergey Yakimchik <siarhei.yakimchyk@regula.by>
2021-09-16 16:34:41 +0300

    fixed description

--- FaceSample/app/src/main/java/com/regula/facesample/items/customization/OverlayCustomItem.java
+++ FaceSample/app/src/main/java/com/regula/facesample/items/customization/OverlayCustomItem.java
@@ -16,10 +16,10 @@ public class OverlayCustomItem extends CategoryItem {
     public void onItemSelected(Context context) {
         // uncomment in res/values/colors.xml:
         // all
-        // reg_face_overlay_background_dark
-        // reg_face_overlay_background_white
-        // reg_face_overlay_border_default
-        // reg_face_overlay_border_active
+        // facesdk_overlay_background_dark
+        // facesdk_overlay_background_white
+        // facesdk_overlay_border_default
+        // facesdk_overlay_border_active
         FaceSDK.Instance().presentFaceCaptureActivity(context, faceCaptureResponse -> { });
     }
 
